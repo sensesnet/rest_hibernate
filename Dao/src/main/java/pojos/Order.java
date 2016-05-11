abdc75d56633a61091d95041a6968e19aaa2b0fe
Kirill Degtyarev <sensesnet@gmail.com>
2016-05-11 03:15:45 +0300

    first commit

--- Dao/src/main/java/pojos/Order.java
+++ Dao/src/main/java/pojos/Order.java
@@ -1,11 +1,21 @@
 package pojos;
 
+import javax.persistence.*;
+
 /**
  * Created by KIRILL on 08.05.2016.
  */
+
+@Entity                   //  annotation describe DB table how javaclass
+@Table(name = "Order")       //  table name
 public class Order {
+    @Id
+    @Column(name = "id")      //autoincrement identify explicitly column
+    @GeneratedValue(strategy = GenerationType.AUTO)  // value autogenerate
     private int id;
+    @Column(name = "mealId")
     private String mealId;
+    @Column(name = "userId")
     private String userId;
 
     public int getId() {
