f84753169c4da5ddd2e3ab4614762a20d1c871b5
Kirill Degtyarev <sensesnet@gmail.com>
2016-05-15 23:53:49 +0300

    first commit

--- Dao/src/main/java/loader/MenuLoader.java
+++
@@ -1,7 +0,0 @@
-package loader;
-
-/**
- * Created by KIRILL on 08.05.2016.
- */
-public class MenuLoader {
-}
--- Dao/src/main/java/pojos/Order.java
+++ Dao/src/main/java/pojos/Order.java
@@ -5,13 +5,21 @@ import javax.persistence.*;
 /**
  * Created by KIRILL on 08.05.2016.
  */
-
+@Entity                   //  annotation describe DB table how javaclass
+@Table(name = "Order")       //  table name
 public class Order {
 
+    @Id
+    @Column(name = "id")      //autoincrement identify explicitly column
+    @GeneratedValue(strategy = GenerationType.AUTO)  // value autogenerate
     private int id;
+    @Column(name = "orderId")
     private int orderId;
+    @Column(name = "mealId")
     private int mealId;
+    @Column(name = "userId")
     private int userId;
+    @Column(name = "Status")
     private OrderStatus orderStatus;
 
     public int getId() {
